7343be75506b6ccb9df625699708736d2924a04d
sanjoymajee02 <sanjoymajee02@gmail.com>
2023-06-21 12:50:42 +0530

    style and login changes

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const reviewButtonElement = document.querySelector("#review-button");
-const reviewFormElement = document.querySelector("#review-form");
 const rateCardElement = document.querySelector("#rate-card");
 const thankYouCardElement = document.querySelector("#thank-you-card");
 const userRatingElement = document.querySelector("#user-rating");
+reviewButtonElement.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+});
 reviewButtonElement.addEventListener("click", (e) => {
   e.preventDefault();
-  const selectedRatingElement = document.querySelector(
-    'input[name="rate"]:checked'
-  );
-  if (!selectedRatingElement) {
-    alert("Please select a rating");
-    return;
-  }
+  const activeButton = document.activeElement;
   rateCardElement.classList.remove("show_content");
   rateCardElement.classList.add("hide_content");
   thankYouCardElement.classList.remove("hide_content");
   thankYouCardElement.classList.add("show_content");
-  userRatingElement.innerHTML = selectedRatingElement.value;
+  userRatingElement.innerHTML = activeButton.value;
 });
